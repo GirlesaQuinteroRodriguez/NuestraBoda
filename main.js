9d992683b291eaa8d72e54242cfa41e7166929df
Girlesa Quintero Rodriguez <girlesa.quinteror@perficient.com>
2024-02-21 19:15:11 -0500

    organizando primera parte de la asistencia

--- main.js
+++ main.js
@@ -15,30 +15,30 @@ var loader;
 var x;
 var y;
 
-function materialClick(event) {
+function materialClick1(event) {
   bg = document.querySelector('.bg');
   bg2 = document.querySelector('.bg2');
   topc = document.querySelector('.top');
   calendar = document.querySelector('.calendar');
   content = document.querySelector('.content');
   controls = document.querySelector('.controls');
-  circle = document.querySelector('.circle');
-  bc = document.querySelector('.button-container');
-  button = document.querySelector('.button');
-  signIn = document.querySelector('.sign-in');
-  loader = document.querySelector('.loader');
-  x = event.offsetX - circle.offsetWidth / 2;
-  y = event.offsetY - circle.offsetHeight / 2;
+  circle1 = document.querySelector('.circle1');
+  bc1 = document.querySelector('.button-container1');
+  button1 = document.querySelector('.button1');
+  signIn1 = document.querySelector('.sign-in1');
+  loader1 = document.querySelector('.loader1');
+  x = event.offsetX - circle1.offsetWidth / 2;
+  y = event.offsetY - circle1.offsetHeight / 2;
   
-  circle.classList.remove('animate');
-  circle.style.left = x + 'px';
-  circle.style.top = y + 'px';
-  bc.style.zIndex = 1;
+  circle1.classList.remove('animate1');
+  circle1.style.left = x + 'px';
+  circle1.style.top = y + 'px';
+  bc1.style.zIndex = 1;
   setTimeout(function() {
-    circle.classList.add('animate');
-    button.classList.add('animate');
-    signIn.classList.add('animate');
-    loader.classList.add('animate');
+    circle1.classList.add('animate1');
+    button1.classList.add('animate1');
+    signIn1.classList.add('animate1');
+    loader1.classList.add('animate1');
     setTimeout(function() {
       loader.classList.add('animateOut');
     }, 1000);
@@ -55,12 +55,58 @@ function materialClick(event) {
   }, 50);
 }
 
+function materialClick2(event) {
+  bg = document.querySelector('.bg');
+  bg2 = document.querySelector('.bg2');
+  topc = document.querySelector('.top');
+  calendar = document.querySelector('.calendar');
+  content = document.querySelector('.content');
+  controls = document.querySelector('.controls');
+  circle2 = document.querySelector('.circle2');
+  bc2 = document.querySelector('.button-container2');
+  button2 = document.querySelector('.button2');
+  signIn2 = document.querySelector('.sign-in2');
+  loader2 = document.querySelector('.loader2');
+  x = event.offsetX - circle2.offsetWidth / 2;
+  y = event.offsetY - circle2.offsetHeight / 2;
+  
+  circle2.classList.remove('animate2');
+  circle2.style.left = x + 'px';
+  circle2.style.top = y + 'px';
+  bc2.style.zIndex = 1;
+  setTimeout(function() {
+    circle2.classList.add('animate2');
+    button2.classList.add('animate2');
+    signIn2.classList.add('animate2');
+    loader2.classList.add('animate2');
+    setTimeout(function() {
+      loader.classList.add('animateOut2');
+    }, 1000);
+    setTimeout(function() {
+      controls.classList.add('hidden');
+      bg.classList.add('no-image');
+      bg2.classList.add('visible');
+      topc.classList.add('visible'); 
+      calendar.classList.add('visible');
+    }, 1460);
+    setTimeout(function() {
+      content.classList.add('removed')
+    }, 2000);
+  }, 50);
+}
+
+
 function restart() {
-  bc.style.zIndex = 0;
-  button.classList.remove('animate');
-  signIn.classList.remove('animate');
-  loader.classList.remove('animate');
-  loader.classList.remove('animateOut');
+  bc1.style.zIndex = 0;
+  bc2.style.zIndex = 0;
+  button1.classList.remove('animate1');
+  button2.classList.remove('animate2');
+  signIn1.classList.remove('animate1');
+  loader1.classList.remove('animate1');
+  loader1.classList.remove('animateOut1');
+  signIn2.classList.remove('animate2');
+  loader2.classList.remove('animate2');
+  loader2.classList.remove('animateOut2');
   controls.classList.remove('hidden');
   bg.classList.remove('no-image');
   bg2.classList.remove('visible');
@@ -69,3 +115,4 @@ function restart() {
   content.classList.remove('removed');
   document.querySelectorAll('.input').forEach(input => input.value = '');
 }
+
